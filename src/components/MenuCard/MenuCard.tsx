e8acfef0851e4e71381d1d35e026793a6c9f516d
Mikael MOREAU <mikaelmoreau@yahoo.fr>
2021-11-11 18:06:03 +0100

    menu card overlay

--- src/components/MenuCard/MenuCard.tsx
+++ src/components/MenuCard/MenuCard.tsx
@@ -4,6 +4,9 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import "./MenuCard.scss";
 
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+
 interface MenuCardProps {
   img: string;
   title: string;
@@ -13,18 +16,37 @@ interface MenuCardProps {
 const MenuCard = (props: MenuCardProps) => {
   const { img, title, onClickEvent } = props;
 
-  return (
-    <div className='menu-card'>
-      <Card sx={{ maxWidth: 345 }} onClick={onClickEvent}>
-        <CardMedia component='img' height='50%' image={img} alt='hamburger' />
-        <CardContent style={{ textAlign: "center" }}>
-          <Typography gutterBottom variant='h5' component='div'>
-            {title}
-          </Typography>
-        </CardContent>
-      </Card>
-    </div>
-  );
+  const theme = useTheme();
+  const mobileScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  if (mobileScreen) {
+    return (
+      <div className='menu-card'>
+        <Card sx={{ maxWidth: 360 }} onClick={onClickEvent}>
+          <CardMedia component='img' image={img} alt='hamburger' />
+          <CardContent style={{ textAlign: "center" }}>
+            <Typography gutterBottom variant='h5' component='div'>
+              {title}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  } else {
+    return (
+      <div style={{ width: "70%" }}>
+        <div className='overlay-image menu-card' onClick={onClickEvent}>
+          <img className='image' src={img} alt='Alt text' />
+          <div className='normal'>
+            <div className='text'></div>
+          </div>
+          <div className='hover'>
+            <div className='text'>{title}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default MenuCard;
