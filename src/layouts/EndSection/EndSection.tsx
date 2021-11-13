fd5325e507529ca53fd7482b3eae15ea1166e9b9
Mikael MOREAU <mikaelmoreau@yahoo.fr>
2021-11-13 07:48:47 +0100

    end_section finish

--- src/layouts/EndSection/EndSection.tsx
+++ src/layouts/EndSection/EndSection.tsx
@@ -3,8 +3,21 @@ import "./EndSection.scss";
 import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import PhoneAndroidTwoToneIcon from "@mui/icons-material/PhoneAndroidTwoTone";
+import { Grid } from "@mui/material";
+
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+
+// Translate
+import "../../i18n";
+import { useTranslation } from "react-i18next";
 
 const EndSection = () => {
+  const theme = useTheme();
+  const mobileScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  const { t } = useTranslation();
+
   return (
     <div className='end-section'>
       <div
@@ -18,28 +31,46 @@ const EndSection = () => {
           <FacebookTwoToneIcon fontSize='large' />
         </a>
         <a className='nostyle' href='https://www.facebook.com/SnackFrensh/'>
-          <h4>SUIVEZ-NOUS !</h4>
+          <h4>{t("end_section.follow")}</h4>
         </a>
       </div>
-
       <div
         style={{
           display: "flex",
           justifyContent: "center",
-          alignItems: "center",
         }}
       >
-        <h4>CONTACTEZ-NOUS :</h4>
-        <MailOutlineIcon />
-        <a href='mailto:frensh.antibes@gmail.com' className='contact-email'>
-          frensh.antibes@gmail.com
-        </a>
-        <p>|</p>
-        <PhoneAndroidTwoToneIcon />
-        <a href='tel:+33493959620' className='contact-email'>
-          +33493959620
-        </a>
+        <h4>{t("end_section.contact")}</h4>
       </div>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: mobileScreen ? "center" : "flex-end",
+            }}
+          >
+            <MailOutlineIcon />
+            <a href='mailto:frensh.antibes@gmail.com' className='contact'>
+              {t("end_section.email")}
+            </a>
+          </div>
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: mobileScreen ? "center" : "flex-start",
+            }}
+          >
+            <PhoneAndroidTwoToneIcon />
+            <a href='tel:+33493959620' className='contact'>
+              {t("end_section.phone")}
+            </a>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 };
